3c7f2f0ca83dc71c0b3599b05c2ce01cf835809b
李富棚 <1308822159@qq.com>
2017-03-06 15:49:22 +0800

    Initial Commit

---
+++ PositioningAndNavigationTests/PositioningAndNavigationTests.m
@@ -0,0 +1,39 @@
+//
+//  PositioningAndNavigationTests.m
+//  PositioningAndNavigationTests
+//
+//  Created by 蜗牛 on 2017/3/6.
+//  Copyright © 2017年 关于蜗牛：一枚九零后码农  蜗牛-----QQ:3197857495-----李富棚  个人微信：woniu1308822159  微信公众号：woniuxueios  简书：蜗牛学IOS  地址：http://www.jianshu.com/users/a664a9fcb096/latest_articles  简书专题：蜗牛学IOS  地址：http://www.jianshu.com/collection/bfcf49bf5d27    GitHub:https://github.com/LiFuPeng     开源中国：https://my.oschina.net/SnailLi  蜗牛. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PositioningAndNavigationTests : XCTestCase
+
+@end
+
+@implementation PositioningAndNavigationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
